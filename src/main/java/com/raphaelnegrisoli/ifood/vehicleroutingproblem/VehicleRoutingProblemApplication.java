628b6435bd0d179db90c65d91bfc2a89afd46712
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-02-21 20:25:30 -0300

    Add spring-boot

---
+++ src/main/java/com/raphaelnegrisoli/ifood/vehicleroutingproblem/VehicleRoutingProblemApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelnegrisoli.ifood.vehicleroutingproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRoutingProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRoutingProblemApplication.class, args);
+	}
+
+}
